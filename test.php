c9d530dba485d122c3d0b95edbaf464d0583c596
V-Foure <90181887+V-Foure@users.noreply.github.com>
2022-02-24 14:22:24 +0100

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$reponse = $pdo->query("select * from Cafetiere where id = ".$id)
-$tab= $reponse->fetch();
-$this->prix = $tab["prix"];
-$this->couleur = $tab["couleur"];
-$this->model = $tab["model"];
-?>
